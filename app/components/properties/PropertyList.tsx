fdf4d78469fa31bbe607e2e0334d04b64862eddb
unknown <jersainmarin20679@gmail.com>
2025-03-04 09:57:37 -0600

    04 List properties on the front page

---
+++ app/components/properties/PropertyList.tsx
@@ -0,0 +1,13 @@
+import PropertyListItem from "./PropertyListitem";
+
+const PropertyList = () => {
+    return (
+        <>
+            <PropertyListItem />
+            <PropertyListItem />
+            <PropertyListItem />
+        </>
+    )
+}
+
+export default PropertyList;
