4142fec75267dc7991c986e7c1fa5e3b07236e75
unknown <jersainmarin20679@gmail.com>
2025-04-02 00:29:24 -0600

    35 Fix the refresh tokens

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,45 +2,89 @@
 
 import { cookies } from "next/headers";
 
-export async function handleLogin(userId: string, accessToken: string, refreshToken: string) {
-    (await cookies()).set('session_userid', userId, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        maxAge: 60 * 60 * 24 * 7, // One week
-        path: '/'
-    });
+export async function handleRefresh() {
+    console.log('handleRefresh');
+    const refreshToken=await getRefreshToken();
+    const token=await fetch('http://localhost:8000/api/auth/token/refresh/',{
+        method:'POST',
+        body:JSON.stringify({
+            refresh:refreshToken
+        }),
+        headers: {
+            'Accept':'application/json',
+            'Content-Tyoe':'application/json'
+        }
+    })
+        .then(response=>response.json())
+        .then(async (json)=> {
+            console.log('Response - Refresh:', json);
+            if (json.access) {
+                (await cookies()).set('session_access_token', json.access,{
+                    httpOnly:true,
+                    secure:process.env.NODE_ENV === 'production',
+                    maxAge:60 * 60, 
+                    path:'/'
+                });
 
-    (await cookies()).set('session_access_token', accessToken, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        maxAge: 60 * 60, // 60 minutes
-        path: '/'
-    });
+                return json.access;
+            } else {
+                resetAuthCookies();
+            }
+        })
+        .catch((error)=>{
+            console.log('error', error);
+            resetAuthCookies();
+        });
 
-    (await cookies()).set('session_refresh_token', refreshToken, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        maxAge: 60 * 60 * 24 * 7, // One week
-        path: '/'
+    return token;
+}
+
+export async function handleLogin(userId:string,accessToken:string,refreshToken:string) {
+    (await cookies()).set('session_userid',userId,{
+        httpOnly:true,
+        secure:process.env.NODE_ENV === 'production',
+        maxAge:60 * 60 * 24 * 7, 
+        path:'/'
     });
+    (await cookies()).set('session_access_token', accessToken,{
+        httpOnly:true,
+        secure:process.env.NODE_ENV === 'production',
+        maxAge:60 * 60, 
+        path:'/'
+    });
+    (await cookies()).set('session_refresh_token', refreshToken,{
+        httpOnly:true,
+        secure:process.env.NODE_ENV === 'production',
+        maxAge:60 * 60 * 24 * 7, 
+        path:'/'
+    });
+    
 }
 
 export async function resetAuthCookies() {
-    (await cookies()).set('session_userid', '');
-    (await cookies()).set('session_access_token', '');
-    (await cookies()).set('session_refresh_token', '');
+    (await cookies()).set('session_userid','');
+    (await cookies()).set('session_access_token','');
+    (await cookies()).set('session_refresh_token','');
 }
 
 //
-// Get data
-
+// Recibir datos
 export async function getUserId(){
-    const userId = (await cookies()).get('session_userid')?.value;
-    return userId ? userId : null;
+    const userId=(await cookies()).get('session_userid')?.value;
+    return userId ? userId : null
 }
 
-export async function getAccessToken(){
-    let accessToken = (await cookies()).get('session_access_token')?.value;
+export async function getAccessToken() {
+    let accessToken=(await cookies()).get('session_access_token')?.value;
+
+    if(!accessToken) {
+        accessToken=await handleRefresh();
+    }
 
     return accessToken;
+}
+
+export async function getRefreshToken() {
+    let refreshToken=(await cookies()).get('session_refresh_token')?.value;
+    return refreshToken;
 }
